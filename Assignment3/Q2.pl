4cb9279d29f54b2899c25e52f439d21028da4039
Mohammad Ali Zahir <m.alizahir786@gmail.com>
2022-11-20 05:19:37 -0500

    Added some more rules to the prolog file

--- Assignment3/Q2.pl
+++ Assignment3/Q2.pl
@@ -2,7 +2,12 @@
 % being active? Create a rule that succeeds by encapsulating any such events into
 % a list, and returns the list.
 
+state(idle).
+state(active)
+state(configuring).
+state(reading).
+state(emergency).
 
-state(configuring, 'Displays configuring mode', 'Enters reading mode', null)
-state(reading, 'Flashes red LED', 'Switches off LED', null )
-state(emergency, 'Produce siren sound', 'Displays exit emergency', reading)
+initial(active).
+final(idle).
+

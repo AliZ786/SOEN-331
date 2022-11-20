5e69c3d66551e6d36e631c72611e88ede43953de
Mohammad Ali Zahir <m.alizahir786@gmail.com>
2022-11-20 05:28:33 -0500

    Added basic rules

--- Assignment3/Q2.pl
+++ Assignment3/Q2.pl
@@ -2,11 +2,11 @@
 % being active? Create a rule that succeeds by encapsulating any such events into
 % a list, and returns the list.
 
-state(idle).
-state(active)
-state(configuring).
-state(reading).
-state(emergency).
+state(idle, null, null, null).
+state(active, null, null, null)
+state(configuring, 'Displays configuring mode', 'Proper threshold values are inputted', null).
+state(reading, 'Flash red LED', 'Turn off LED', null).
+state(emergency, 'Produces siren sound', 'Exits emergency mode', null).
 
 initial(active).
 final(idle).

be332e75fa18cd44aa1846fe8f799cad83eb9bd6
Mohammad Ali Zahir <m.alizahir786@gmail.com>
2022-11-21 01:48:59 -0500

    Added q3 and formatted file

--- Assignment3/Q2.pl
+++ Assignment3/Q2.pl
@@ -1,7 +1,17 @@
-% a) What events, if any, are legal while the system is at any given mode, e.g. while
-% being active? Create a rule that succeeds by encapsulating any such events into
-% a list, and returns the list.
 
+%%  CONCORDIA UNIVERSITY
+%%  Department of Computer Science and Software Engineering
+%%  SOEN 331 Section S:  Assignment 3
+%%  Fall 2022
+%%  Date of submission: November 21, 2022
+%%
+%%  Authors:
+%%
+%%  Mohammad Ali Zahir - ID: 40077619 & Marwa Khalid - ID: 40155098
+
+
+
+% Facts 
 state(idle, null, null, null).
 state(active, null, null, null).
 state(configuring, 'Displays configuring mode', 'Proper threshold values are inputted', null).
@@ -12,16 +22,23 @@ state(exit, 'From idle, system exits', 'System shuts off', null).
 initial(active).
 final(exit).
 
-transition(active, configuring, configure, null, 'Displays configuring mode').
-transition(active, reading, skip-configure, null, 'Perform series of beeps').
+transition(active, configuring, configure, 'Want to set threshold values', 'Displays configuring mode').
+transition(active, reading, skip-configure, 'Skip setting threshold values', 'Perform series of beeps').
 transition(active, idle, idle, null, 'System becomes idle').
-transition(configuring, reading, finish-configuring, null, 'Proper threshold values met, perform series of beeps').
-transition(reading, emergency,values-above-threshold , null, 'System will enter emergency mode').
-transition(reading, configuring ,reset-threshold , null, 'LED lights will shut off').
-transition(emergency, reading ,reset-button-press , null, 'Exit emergency mode/shut off siren').
+transition(configuring, reading, finish-configuring, 'Proper threshold values are met', 'Perform series of beeps').
+transition(reading, emergency,values-above-threshold , 'If CO_value or Temp_Value > threshold_value', 'System will enter emergency mode').
+transition(reading, configuring ,reset-threshold , 'Set new threshold values', 'LED lights will shut off').
+transition(emergency, reading ,reset-button-press , 'Stop siren sound', 'Exit emergency mode').
 transition(idle, active ,active , null, 'System enters active mode').
 transition(idle, exit ,exit , null, 'System shuts off').
 
+
+
+% --------- Questions ----------
+
+% a) What events, if any, are legal while the system is at any given mode, e.g. while
+% being active? Create a rule that succeeds by encapsulating any such events into
+% a list, and returns the list.
 
 
 
@@ -40,3 +57,13 @@ actions(State1) :-
             ActionList), write(ActionList).
 
 
+%(c) Create a rule that for a given pair of source-destination states, the rule succeeds
+% by returning a list of criteria under which the system can perform a transition
+% from the source to the destination. The criteria are defined as event-guard
+% pairs.
+
+event_guard_pairs(Set, State1, State2) :-
+    findall([Event, Guard],
+            (transition(State1, State2,Event,Guard , _), State1 \== State2),
+            List),
+    		list_to_set(List,Set). 

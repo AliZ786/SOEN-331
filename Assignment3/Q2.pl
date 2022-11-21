adc317abfd970f34b5905b714d52bc0737f1afa5
Mohammad Ali Zahir <m.alizahir786@gmail.com>
2022-11-20 23:08:07 -0500

    Added Q2

--- Assignment3/Q2.pl
+++ Assignment3/Q2.pl
@@ -7,17 +7,20 @@ state(active, null, null, null).
 state(configuring, 'Displays configuring mode', 'Proper threshold values are inputted', null).
 state(reading, 'Flash red LED', 'Turn off LED', null).
 state(emergency, 'Produces siren sound', 'Exits emergency mode', null).
+state(exit, 'From idle, system exits', 'System shuts off', null).
 
 initial(active).
-final(idle).
+final(exit).
 
 transition(active, configuring, configure, null, 'Displays configuring mode').
 transition(active, reading, skip-configure, null, 'Perform series of beeps').
-transition(active, idle, idle, null, 'On idle, system can shut down').
+transition(active, idle, idle, null, 'System becomes idle').
 transition(configuring, reading, finish-configuring, null, 'Proper threshold values met, perform series of beeps').
 transition(reading, emergency,values-above-threshold , null, 'System will enter emergency mode').
 transition(reading, configuring ,reset-threshold , null, 'LED lights will shut off').
 transition(emergency, reading ,reset-button-press , null, 'Exit emergency mode/shut off siren').
+transition(idle, active ,active , null, 'System enters active mode').
+transition(idle, exit ,exit , null, 'System shuts off').
 
 
 
@@ -30,3 +33,10 @@ events(State1) :-
 
 % (b) Create a rule that succeeds by collecting all system actions into a list and returns
 % such list.
+
+actions(State1) :-
+    findall([Action],
+            transition(State1,_,_,_,Action),
+            ActionList), write(ActionList).
+
+

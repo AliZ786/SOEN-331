7a26bdd22bd1b58cbfacdf567a8cd4f9f7bd8286
AliZ786 <59709752+AliZ786@users.noreply.github.com>
2022-11-20 19:45:40 -0500

    Finished question a

--- Assignment3/Q2.pl
+++ Assignment3/Q2.pl
@@ -3,7 +3,7 @@
 % a list, and returns the list.
 
 state(idle, null, null, null).
-state(active, null, null, null)
+state(active, null, null, null).
 state(configuring, 'Displays configuring mode', 'Proper threshold values are inputted', null).
 state(reading, 'Flash red LED', 'Turn off LED', null).
 state(emergency, 'Produces siren sound', 'Exits emergency mode', null).
@@ -11,3 +11,18 @@ state(emergency, 'Produces siren sound', 'Exits emergency mode', null).
 initial(active).
 final(idle).
 
+transition(active, configuring, configure, null, 'Displays configuring mode').
+transition(active, reading, skip-configure, null, 'Perform series of beeps').
+transition(active, idle, idle, null, 'On idle, system can shut down').
+transition(configuring, reading, finish-configuring, null, 'Proper threshold values met, perform series of beeps').
+transition(reading, emergency,values-above-threshold , null, 'System will enter emergency mode').
+transition(reading, configuring ,reset-threshold , null, 'LED lights will shut off').
+transition(emergency, reading ,reset-button-press , null, 'Exit emergency mode/shut off siren').
+
+
+
+
+events(State1) :-
+    findall([Event],
+            transition(State1,_,Event, _, _), 
+            EventList), write(EventList).
